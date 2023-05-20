154dafcc49bfbb8f41fd87b803a7cd1a740e279e
jrrelucio <jrrelucio@up.edu.ph>
2023-05-20 15:58:35 +0800

    Feat: Access API

--- wordleclone/src/wordleclone/app.py
+++ wordleclone/src/wordleclone/app.py
@@ -4,6 +4,7 @@ My first application
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+import httpx
 
 def greeting(name):
     if name:
@@ -39,10 +40,14 @@ class WordleClone(toga.App):
         self.main_window.show()
 
     def say_hello(self, widget):
+        with httpx.Client() as client:
+            response = client.get("https://jsonplaceholder.typicode.com/posts/42")
+
+        payload = response.json()
+
         self.main_window.info_dialog(
             greeting(self.name_input.value),
-            "Hi there!",
+            payload["body"],
         )
-
 def main():
     return WordleClone()
